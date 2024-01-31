34f150725ba08bf3f5219d83b9b89f5c46a47620
Ana Luisa <luisauribeblum@gmail.com>
2024-01-31 13:14:50 -0500

    24

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,16 @@ import DeleteSuccess from './components/DeleteSuccess/DeleteSuccess.jsx'
 
 function App() {
 
-  
+
   const [userUpdate, setUserUpdate] = useState()
   const [isFormClose, setIsFormClose] = useState(true)
   const [isDeleteSuccess, setisDeleteSuccess] = useState(false)
 
 
   const baseUrl = 'https://users-crud.academlo.tech'
-  const [ users, getUser, createUser, deleteUser, updateUser ] = useFetch(baseUrl)
+  const [users, getUser, createUser, deleteUser, updateUser] = useFetch(baseUrl)
 
-  useEffect(()=>{
+  useEffect(() => {
     getUser()
   }, [])
 
@@ -25,40 +25,39 @@ function App() {
     setIsFormClose(false)
   }
 
-  if (isDeleteSuccess) {
-    return <div className='delete__success-container'>
-      <DeleteSuccess 
-      setisDeleteSuccess={setisDeleteSuccess}
-      /></div>;
- }
 
   return (
-    <div> 
+    <div>
       <h1 className='title-app' > Users APP </h1>
       <button onClick={handleOpenForm} className="app__button-open-form"> + Create New User </button>
       <div className={`form__container ${isFormClose && 'form__close'}`}>
-      <FormUser
-        createUser={createUser}
-        userUpdate={userUpdate}
-        updateUser={updateUser}
-        setUserUpdate={setUserUpdate}
-        setIsFormClose={setIsFormClose}
-      />
-      </div>
-    <div className="user-container">
-      {
-        users?.map(user=>(
-          <UserCard
-          key={user.id}
-          user={user}
-          deleteUser={deleteUser}
+        <FormUser
+          createUser={createUser}
+          userUpdate={userUpdate}
+          updateUser={updateUser}
           setUserUpdate={setUserUpdate}
           setIsFormClose={setIsFormClose}
-          setisDeleteSuccess={setisDeleteSuccess}
-          isDeleteSuccess={isDeleteSuccess}
-          />
+        />
+      </div>
+      <div className="user-container">
+        {
+          users?.map(user => (
+            <UserCard
+              key={user.id}
+              user={user}
+              deleteUser={deleteUser}
+              setUserUpdate={setUserUpdate}
+              setIsFormClose={setIsFormClose}
+              setisDeleteSuccess={setisDeleteSuccess}
+              isDeleteSuccess={isDeleteSuccess}
+            />
           ))
-      }
+        }
+      </div>
+      <div className={`delete__success-container ${!isDeleteSuccess && 'delete-close'}`}>
+        <DeleteSuccess
+          setisDeleteSuccess={setisDeleteSuccess}
+        />
       </div>
     </div>
   )
